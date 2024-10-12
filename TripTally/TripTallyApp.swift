08ef7018b14841a087a4c472a10c20fe65487920
Martin Flischman <martincodes@icloud.com>
2024-10-12 19:51:33 +0200

    Initial Commit

---
+++ TripTally/TripTallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripTallyApp.swift
+//  TripTally
+//
+//  Created by Martin on 2024/10/12.
+//
+
+import SwiftUI
+
+@main
+struct TripTallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
